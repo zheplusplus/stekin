c3a253b50687fd1a16d55b1d838cec0dcab42d6b
Neuron Teckid <Lene13@gmail.com>
2011-05-02 15:20:04 +0800

    built-in: WriteExprInst moved to instance

--- proto/built-in.cpp
+++ proto/built-in.cpp
@@ -1,41 +1,9 @@
 #include "built-in.h"
-#include "inst-mediates.h"
-#include "function.h"
-#include "../instance/node-base.h"
-#include "../instance/expr-nodes.h"
-#include "../instance/block.h"
-#include "../instance/type.h"
-#include "../output/statement-writer.h"
+#include "../instance/built-in.h"
 
 using namespace proto;
 
-namespace {
-
-    struct WriteExprInst
-        : public inst::Expression
-    {
-        explicit WriteExprInst(util::sptr<inst::Expression const> e)
-            : expr(std::move(e))
-        {}
-
-        util::sref<inst::Type const> typeof() const
-        {
-            return inst::Type::BIT_VOID;
-        }
-
-        void write() const
-        {
-            output::beginWriteStmt();
-            expr->write();
-            output::endWriteStmt();
-        }
-
-        util::sptr<inst::Expression const> const expr;
-    };
-
-}
-
 util::sptr<inst::Expression const> WriteExpr::inst(util::sref<inst::Scope> scope) const
 {
-    return util::mkptr(new WriteExprInst(ref.inst(scope)));
+    return util::mkptr(new inst::WriteExpr(ref.inst(scope)));
 }
